3a5090f9b4afe999cbe466a1b66c34e7297383d4
felipereina <reina.felipe@gmail.com>
2019-11-21 21:24:35 +0000

    added toggleButton and sideMenu for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,33 @@ import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends React.Component {
 
-export default layout
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerCloseHandler = () =>{
+        this.setState({showSideDrawer: false})
+    } 
+
+    sideDrawerToggleHandler = () =>{
+        this.setState((prevState) =>{
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+    
+    render() {
+        return (<Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+            <div>Toolbar, SideDrawer, Backdrop</div>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+}
+
+export default Layout
